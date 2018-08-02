2d73a1907f56516a1fb804fd5fe553cc5b0f6a84
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-08-02 22:59:41 +1200

    Random gradient background, font change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './scss/index.scss'
 
-console.log('grumbling watermelon')
-const sayHi = () => console.log('hey there')
+const gradientEl = document.getElementById('gradient-target')
+
+const num = Math.floor(Math.random() * 10) + 1
+const gradientClass = `gradient-${num}`
+
+gradientEl.className = gradientClass
