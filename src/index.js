acd9c8dd569868711e319a568cac13a3017d5eb4
Michael Lowe <michael.lowe@education.govt.nz>
2018-08-03 12:33:57 +1200

    Add animation to titles

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ const num = Math.floor(Math.random() * 10) + 1
 const gradientClass = `gradient-${num}`
 
 gradientEl.className = gradientClass
+
+setTimeout(() => {
+    const nameEl = document.getElementById('name')
+    nameEl.classList += 'fadeInDown'
+}, 500)
+
+setTimeout(() => {
+    const nameEl = document.getElementById('subtitle')
+    nameEl.classList += 'fadeInDown'
+}, 700)
