f2321664adbb4b87ad86d8bae68ba28cf9e6609a
michael-lowe-nz <mikkeyfluttie@gmail.com>
2018-08-13 23:54:44 +1200

    It draws

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './scss/index.scss'
 
+import Vivus from 'vivus'
+
 const gradientEl = document.getElementById('gradient-target')
 
 const num = Math.floor(Math.random() * 13) + 1
@@ -10,14 +12,18 @@ gradientEl.className = gradientClass
 setTimeout(() => {
     const nameEl = document.getElementById('name')
     nameEl.classList += 'fadeInDown'
-}, 500)
+}, 1500)
 
 setTimeout(() => {
     const nameEl = document.getElementById('subtitle')
     nameEl.classList += 'fadeInDown'
-}, 700)
+}, 1700)
 
 setTimeout(() => {
     const iconEls = document.querySelectorAll('img')
     iconEls.forEach(el => el.classList += 'fadeInLeft');
-}, 800)
+}, 1800)
+
+new Vivus('logo', {
+    duration: 70,
+});
